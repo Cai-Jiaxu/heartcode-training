c92a538390ed76bb81e878465a27e6f077bb5009
Fegetable <100826140+Fegetable@users.noreply.github.com>
2024-10-11 16:10:18 +0800

    update2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { FlipWords } from "@/components/ui/flip-words";
 
 export default function Home() {
-  const words: string[] = ["skibidi", "litty", "sigma"];
+  const words: string[] = ["bad", "very bad", "very very bad"];
 
   return (
 	<div>
   	<div className="flex flex-col h-[calc(100vh-60px)] items-center justify-center">
-    	<h1 className="font-bold text-5xl">Taking drugs is 
+    	<h1 className="font-bold text-5xl">Drugs are 
       	<FlipWords words={words}/>
     	</h1>
   	</div>
